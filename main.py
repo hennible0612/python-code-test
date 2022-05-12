880fea15b65d90a823070a10b4791d301ceb9ee6
swh <sungwoo166@gmail.com>
2022-05-12 23:35:22 +0900

    deques 추가
    .

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-# collections deque
-from collections import deque
+# n, m = map(int, input().split())
+num = int(input())
+answer = 0
+i = 1
+while True:
 
-data = deque([2, 3, 4])
-data.appendleft(1)
-data.append(5)
-
-print(data)
-print(list(data))
-
-from collections import Counter
-
-counter = Counter(['red', 'blue', 'red', 'green', 'blue', 'blue'])
-print(counter['blue'])
-print(dict(counter))
+    num -= i
+    if num >= 0:
+        answer += 1
+        i += 1
+    else:
+        print(answer)
+        break
